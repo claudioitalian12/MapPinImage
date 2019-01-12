6db74648b6dfc3032e49269204d5f815decf1097
claudioitalian12 <dark.claudio12@gmail.com>
2019-01-12 16:01:38 +0100

    0.0.1

---
+++ MapPinImage/PinValue.swift
@@ -0,0 +1,16 @@
+//
+//  PinValue.swift
+//  MapPinImage
+//
+//  Created by claudio Cavalli on 12/01/2019.
+//  Copyright © 2019 claudio Cavalli. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import SceneKit
+
+struct PinValue {
+    let itemDescription: String
+    let location: CLLocation
+}
--- MapPinImage/ViewController.swift
+++ MapPinImage/ViewController.swift
@@ -7,14 +7,94 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var userLocation: CLLocation?
+    var selectedAnnotation: MKAnnotation?
+    var Pin = [PinValue]()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        setupLocations()
+        
     }
 
-
+    func setupLocations() {
+     
+      for _ in 0..<20 {
+      let coordinate =  CLLocation(latitude: Double.random(in: 30...46), longitude: Double.random(in: 10...15))
+    
+        let pin = PinValue.init(itemDescription: "ImagePin", location: coordinate)
+        Pin.append(pin)
+        
+        }
+        
+        for item in Pin {
+            let annotation = MapAnnotation(location: item.location.coordinate, item: item)
+            self.mapView.addAnnotation(annotation)
+        }
+        
+    }
+    
+    
 }
 
+
+extension ViewController: MKMapViewDelegate {
+    
+   
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+        
+        let annotationIdentifier = "ImagePin"
+        var annotationView: MKAnnotationView?
+        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
+            annotationView = dequeuedAnnotationView
+            annotationView?.annotation = annotation
+        }
+        else {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        if let annotationView = annotationView {
+            
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: String(counter))
+            annotationView.frame.size.width  = 36
+            annotationView.frame.size.height = 39
+            counter += 1
+            
+        }
+        
+        return annotationView
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        self.userLocation = userLocation.location
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+      
+   
+    }
+    
+    
+}
